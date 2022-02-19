3315dde5f7e6dfb1fdb9fede9b9dcb01a2470472
Angelo Schuler Piletti <74366173+AngeloSchulerPiletti@users.noreply.github.com>
2022-02-19 00:03:32 -0300

    starting project as csharp dotnet api

---
+++ HackaXP/Controllers/OpenFinanceController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HackaXP.Controllers
+{
+    [ApiVersion("1")]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    [ApiController]
+    public class OpenFinanceController : ControllerBase
+    {
+        [HttpGet]
+        [Route("demo")]
+        public IActionResult Demo()
+        {
+            return Ok("Our api is working!");
+        }
+    }
+}
